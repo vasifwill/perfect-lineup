b92706adf6d2a37e52e0650ff39cd93dc6133203
vasif mammadov <vasif.will@gmail.com>
2020-07-24 22:44:11 -0400

    lineup

--- tests.js
+++ tests.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect
-const { describe, it } = require('mocha')
+//const { describe, it } = require('mocha')
 const validateLineup = require('./index')
 
 describe('validateLineup', () => {
